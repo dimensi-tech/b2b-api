ca82e978ff6a13ead04a4a77735ced16277f5f99
Persada Ersad <ersad40@gmail.com>
2019-11-23 08:05:50 +0700

    Skip precompile

--- config/deploy.rb
+++ config/deploy.rb
@@ -9,6 +9,7 @@ set :use_sudo, false
 append :linked_files, 'config/database.yml', 'config/secrets.yml', '.env'
 append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets',
        'vendor/bundle', 'public/system', 'public/uploads'
+set :assets_roles, []
 
 namespace :puma do
   desc 'Create Directories for Puma Pids and Socket'
