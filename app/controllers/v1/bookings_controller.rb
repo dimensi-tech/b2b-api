7881808453c2212df6809b0184393a1cfc53d213
Persada Ersad <ersad40@gmail.com>
2019-12-02 23:13:22 +0700

    Update API Booking
     - Assign identity_ids update
    
     - List bookings by customer
    
    Trello: https://trello.com/c/CmJXTmC8

--- app/controllers/v1/bookings_controller.rb
+++ app/controllers/v1/bookings_controller.rb
@@ -1,5 +1,8 @@
+# frozen_string_literal: true
+
 module V1
   class BookingsController < ApplicationController
+    before_action :set_booking, only: %i[assign_identities]
     def create_booking
       @booking = Booking.new(booking_params)
 
@@ -10,11 +13,38 @@ module V1
       end
     end
 
+    def assign_identities
+      if @booking.present? && @booking.update(identities_params)
+        render json: @booking, serializer: BookingSerializer
+      else
+        render json: { success: false, message: 'Error Assign Identity Ids' }
+      end
+    end
+
+    def list_bookings
+      @booking = Booking.find_by(customer_id: params[:customer_id])
+
+      if @booking.present?
+        render json: @booking, serializer: BookingSerializer
+      else
+        render json: { success: false, message: 'Error Assign Identity Ids' }
+      end
+    end
+
     private
+
+    def set_booking
+      @booking = Booking.find_by(id: params[:booking_id])
+    end
 
     def booking_params
       params.require(:booking)
-            .permit(:departure_date, :package_id, :child, :adult, :customer_id)
+            .permit(:departure_date, :package_id, :child,
+                    :adult, :customer_id, identity_ids: [])
+    end
+
+    def identities_params
+      params.require(:booking).permit(identity_ids: [])
     end
   end
 end
--- app/serializers/booking_serializer.rb
+++ app/serializers/booking_serializer.rb
@@ -1,5 +1,6 @@
 class BookingSerializer < ActiveModel::Serializer
-  attributes :id, :departure_date, :package_id, :child, :adult, :number, :customer_id
+  attributes :id, :departure_date, :package_id, :child, :adult,
+             :number, :customer_id, :identity_ids
 
   belongs_to :package
 end
