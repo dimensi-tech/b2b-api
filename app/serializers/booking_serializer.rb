74d010361eafbaf0b3410be30399b36c1da86480
Persada Ersad <ersad40@gmail.com>
2020-04-21 19:18:30 +0700

    add booking type to booking serializer

--- app/serializers/booking_serializer.rb
+++ app/serializers/booking_serializer.rb
@@ -1,7 +1,7 @@
 class BookingSerializer < ActiveModel::Serializer
   attributes :id, :departure_date, :package_id, :voucher_id, :price, :status,
              :number, :customer_id, :identity_ids, :person, :midtrans_id,
-             :product, :passport_ids, :booking_status
+             :product, :passport_ids, :booking_status, :booking_type
 
   belongs_to :package
   belongs_to :voucher
