876c291239f75f30073c19332207df79cc68acb1
MRomadhan <muhammad.romadhan@c-aio.com>
2020-09-23 13:48:50 +0700

    Product Lists
    Product list show by availabilities date package
    
    Trello : https://trello.com/c/04I2ddLk

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,9 +1,13 @@
 class Product < ApplicationRecord
   mount_uploader :image, ImageUploader
+
   belongs_to :category
   belongs_to :travel_destination
+
   has_many :packages
   has_many :package_details, through: :packages
+
+  scope :available_packages, -> { joins(:packages).where('packages.available_date && ?', "{#{Time.now.strftime('%d/%m/%Y')}}") }
 
   def image_path
     "#{ENV['HOST']}#{image_url}"
