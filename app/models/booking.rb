2dfe0582de9dd13bc207e1074431d9f1a532a538
Persada Ersad <ersad40@gmail.com>
2020-10-13 20:30:57 +0700

    add enumerize paid booking status

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -13,7 +13,7 @@ class Booking < ApplicationRecord
   enumerize :booking_type, in: { full: 1, savings: 2 }
   enumerize :status, in: { ordered: 0, paid: 1, error: 2 }
   enumerize :booking_status, in: { pending: 0, booked: 1, modified: 2, cancelled: 3,
-                                   down_payment: 4, saving_progress: 5, completed: 6 }
+                                   down_payment: 4, saving_progress: 5, completed: 6, paid: 7 }
 
   after_create :assign_number
 
