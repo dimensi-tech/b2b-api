30c318c0788ee2a96d5a59ef844baabafccf8c23
Persada Ersad <ersad40@gmail.com>
2020-11-15 16:01:09 +0700

    update fixed create booking undefined data valid booking serializer

--- app/serializers/booking_serializer.rb
+++ app/serializers/booking_serializer.rb
@@ -2,7 +2,7 @@ class BookingSerializer < ActiveModel::Serializer
   attributes :id, :departure_date, :package_id, :voucher_id, :price, :status, :adult_bio_ids, :child_bio_ids,
              :number, :customer_id, :identity_ids, :person, :midtrans_id, :child_passport_ids, :booking_status,
              :product, :passport_ids, :booking_status, :booking_type, :adult, :child, :child_amount_saving,
-             :adult_amount_saving, :dp_amount, :data_valid
+             :adult_amount_saving, :dp_amount
 
   belongs_to :package
   belongs_to :voucher
